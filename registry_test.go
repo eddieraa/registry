9c7f878dcafbb7484ea6b2d2afb7321c72a34365
eddieraa <51861514+eddieraa@users.noreply.github.com>
2020-11-07 10:07:32 +0100

    Dev (#6)
    * Implement Filter
    
    * Adding Filter implementation
    Adding samples
    
    * Implement LoadBalance Filter
    
    * Filter
    
    * Change Filter implementation
    
    * Avoid  segfault when call GetService("xxxx",nil)
    
    * Fix bug with LoadBalancerFilter
    
    * Adding tests
    
    * Adding cache

--- registry_test.go
+++ registry_test.go
@@ -100,3 +100,22 @@ func TestLB(t *testing.T) {
 	s, err = r.GetService(name)
 	logrus.Infof("Service %s", s.Address)
 }
+
+func newService(addr, host, name string) *Pong {
+	return &Pong{Service: Service{Address: addr, Host: host, Name: name}, Timestamps: &Timestamps{Registered: 3, Duration: 5}}
+}
+func TestChainFilter(t *testing.T) {
+	pongs := []*Pong{
+		newService("localhost:32", "localhost", "myservice"),
+		newService("localhost:34", "localhost", "myservice"),
+		newService("localhost:35", "localhost", "myservice"),
+		newService("localhost:36", "localhost", "myservice"),
+	}
+	f := LoadBalanceFilter()
+
+	logrus.Info("\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f))
+	logrus.Info("\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f))
+	logrus.Info("\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f))
+	logrus.Info("\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f), "\n", chainFilters(pongs, f))
+
+}
